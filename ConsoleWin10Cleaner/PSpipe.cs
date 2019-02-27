7eceb05b3f345095df696c4c2b4ddb210c05e56b
MarekMichalski <michalskimarek89@gmail.com>
2019-02-27 12:34:33 +0100

    working app

---
+++ ConsoleWin10Cleaner/PSpipe.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace ConsoleWin10Cleaner
+{
+    class PSpipe
+    {
+        public string RunScript(string scriptText)
+        {
+            Runspace runspace = RunspaceFactory.CreateRunspace();
+            runspace.Open();
+            Pipeline pipeline = runspace.CreatePipeline();
+            pipeline.Commands.AddScript(scriptText);
+            pipeline.Commands.Add("Out-String");
+
+            Collection < PSObject > results = pipeline.Invoke();
+            runspace.Close();
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (PSObject obj in results)
+            {
+                stringBuilder.AppendLine(obj.ToString());
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
---
+++ ConsoleWin10Cleaner/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleWin10Cleaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program will remove the following applications from your system:");
+            Console.WriteLine("3D Builder");
+            Console.WriteLine("Alarms & Clock");
+            Console.WriteLine("Calendar and Mail");
+            Console.WriteLine("Camera");
+            Console.WriteLine("Feedback Hub");
+            Console.WriteLine("Office");
+            Console.WriteLine("Get Started and Tips");
+            Console.WriteLine("Skype");
+            Console.WriteLine("Groove Music, Movies & TV");
+            Console.WriteLine("Maps");
+            Console.WriteLine("Messaging");
+            Console.WriteLine("Microsoft Solitaire Collection");
+            Console.WriteLine("Microsoft Wallet");
+            Console.WriteLine("Microsoft Wi-Fi");
+            Console.WriteLine("Money, News, Sports and Weathe");
+            Console.WriteLine("OneNote");
+            Console.WriteLine("Paid Wi-Fi & Cellular");
+            Console.WriteLine("Paint ");
+            Console.WriteLine("People");
+            Console.WriteLine("Phone and Phone Companion");
+            Console.WriteLine("Photos");
+            Console.WriteLine("Sticky Notes");
+            Console.WriteLine("Sway");
+            Console.WriteLine("View 3D");
+            Console.WriteLine("Windows Holographic");
+            Console.WriteLine("Voice Recorder");
+            Console.WriteLine("Windows Store");
+            Console.WriteLine("Xbox");
+            Console.WriteLine("Do you want to continue? Y/N");
+
+            List<string> appsArray = new List<string>(new string[] {
+                "3dbuilder",
+                "alarms",
+                "communicationsapps",
+                "camera",
+                "feedback",
+                "officehub",
+                "getstarted",
+                "skypeapp",
+                "zune",
+                "maps",
+                "messaging",
+                "solitaire",
+                "wallet",
+                "connectivitystore",
+                "bing",
+                "onenote",
+                "oneconnect",
+                "mspaint",
+                "people",
+                "phone",
+                "photos",
+                "sticky",
+                "sway",
+                "3d",
+                "soundrecorder",
+                "holographic",
+                "windowsstore",
+                "xbox" });
+
+            PSpipe pspipe = new PSpipe();
+            ConsoleKeyInfo result = Console.ReadKey();
+            if (result.KeyChar == 'y' || result.KeyChar == 'Y')
+            {
+                foreach (string app in appsArray)
+                {
+                    Console.WriteLine("Removing applications...");
+                    Console.Write(pspipe.RunScript($"get-appxpackage *{app}* | remove-appxpackage"));
+                }
+            }
+            else Environment.Exit(1);
+        }
+    }
+}
